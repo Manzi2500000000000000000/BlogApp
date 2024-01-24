060e92cf2555763ef63b5c23a39040736fcfca86
Manzi <manzidan2003@gmail.com>
2024-01-24 11:16:48 +0200

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -40,37 +40,3 @@ function App() {
 export default App;
 
 
-// import React from 'react';
-// import TopBar from "./topbar/TopBar.jsx";
-// import Home from "./header/pages/Home.jsx";
-// import Single from './header/pages/single/Single.jsx';
-// import Write from './header/pages/write/Write.jsx';
-// import Settings from './header/pages/settings/Settings.jsx';
-// import Login from './header/pages/login/Login.jsx';
-// import Register from './header/pages/register/Register.jsx';
-// import{BrowserRouter , Routes,Route, Link } from "react-router-dom";
-
-
-// function App() {
-//   const user = false;
-//   return(
-//     <BrowserRouter>
-//     <TopBar />
-
-//     <Routes>
-//       <Route exact path="/" element={<Home />} />
-//       <Route  path="/login" element={user? <Home/> :<Login />} />
-//       <Route  path="/register" element={user ? <Home/> :<Register />} />
-//       <Route  path="/write" element={user? <Register />: <Write/>} />
-//       <Route  path="/settings" element={user? <Settings/>: <Register/>} />
-//       <Route  path="/post/:postId" element={<Single />} />
-
-
-//     </Routes>
-    
-//   </BrowserRouter>
-//   );
-  
-// }
-
-// export default App;
